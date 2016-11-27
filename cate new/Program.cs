cabca62b227424f83e5f7e4eced5e060d083fdee
Obsidian <Obsidian@DESKTOP-V00G718>
2016-11-27 12:50:13 +0800

    添加项目文件。

---
+++ cate new/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cate_new
+{
+    public delegate void Water_herter(int Dwp);
+    class Program
+    {
+       static int Dwp;
+       static public void Boilwater(int Dwp)
+        {
+            Console.WriteLine("开始升温");
+            for (Dwp = 37; Dwp < 80; Dwp++)
+            {
+                Console.WriteLine("水温已经达到了" + Dwp);
+            }
+            Console.WriteLine("都80°了，你丫是想煮肉啊");
+        }
+        public static void dwp(Water_herter Boilwater)
+        {
+            Boilwater(Dwp);
+        }
+        static void Main(string[] args)
+        {
+            dwp(Boilwater);
+
+        }
+    }
+}
